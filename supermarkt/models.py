d1706145c929ffc9524cdbc253224a773cae4490
Paul <peru77@gmx.at>
2017-11-25 22:00:42 +0100

    erstes modell

--- supermarkt/models.py
+++ supermarkt/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Produkt(models.Model):
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.CharField(max_length=200, null=True)
+
+
+class Company(models.Model):
+
+    name = models.CharField(max_length=200, null=True)
+
