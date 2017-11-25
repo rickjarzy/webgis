18ba3b396e646058e89f5bd326a22215b2e65d58
Paul <peru77@gmx.at>
2017-11-25 22:44:09 +0100

    update

--- supermarkt/admin.py
+++ supermarkt/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class ProduktAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'price')
+
+
+class ProduktTypeAdmin(admin.ModelAdmin):
+
+    list_display = ('name',)
+
+class CompanyAdmin(admin.ModelAdmin):
+
+    list_display = ('name',)
+
+
+admin.site.register(Produkt, ProduktAdmin)
+admin.site.register(ProduktType, ProduktTypeAdmin)
+admin.site.register(Company, CompanyAdmin)
--- supermarkt/models.py
+++ supermarkt/models.py
@@ -2,13 +2,30 @@ from django.db import models
 
 # Create your models here.
 
+
 class Produkt(models.Model):
 
-    name = models.CharField(max_length=200, null=True)
-    price = models.CharField(max_length=200, null=True)
+    class Meta:
+        verbose_name_plural = "Produkte"
+        db_table = 'webgis_produkt'
+
+    name = models.CharField(max_length=200, null=True, blank=True)
+    company = models.ForeignKey('Company', null=True, blank=True)
+    price = models.CharField(max_length=200, null=True, blank=True)
+    type = models.ForeignKey('ProduktType', null=True, blank=True)
 
 
 class Company(models.Model):
+    class Meta:
+        db_table = 'webgis_company'
+        verbose_name_plural = "Companies"
 
-    name = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
 
+
+class ProduktType(models.Model):
+    class Meta:
+        db_table = 'webgis_produkttype'
+        verbose_name_plural = "Produkttypen"
+
+    name = models.CharField(max_length=200, null=True, blank=True)
